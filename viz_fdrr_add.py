cbcb0839a481ad3d3ee21796a55b613a2a1771d8
meenadeviimuralikumar <mdmkumar94@gmail.com>
2025-05-22 13:41:44 -0700

    Update viz_fdrr_add.py with app server for Render

--- viz_fdrr_add.py
+++ viz_fdrr_add.py
@@ -9,7 +9,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Initialize the app
 app = Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 
 # App layout
 app.layout = html.Div(children=[
